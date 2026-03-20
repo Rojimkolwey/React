916248b3b83e89c5610d3ef1c78125433c96192c
rojimkolwey <rodgersmkolwe@gmail.com>
2026-03-20 22:53:53 +0300

    helloe world

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <div style={{textAlign: "center", marginTop: "50px"}}>
-      <h1>Hello React 🚀</h1>
-      <p>I am learning React!</p>
-      <button onClick={() => alert("React is working!")}>
-        Click Me
-      </button>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
