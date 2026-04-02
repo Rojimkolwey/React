b2bb76b5a6c4f7188c4c1758fddaa21f530d5508
rojimkolwey <rodgersmkolwe@gmail.com>
2026-04-02 23:02:37 +0300

    components

---
+++ myapp/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <p>Made by Rodgers © 2025</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ myapp/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <p>Learning React one day at a time</p>
+    </div>
+  );
+}
+
+export default Header;
