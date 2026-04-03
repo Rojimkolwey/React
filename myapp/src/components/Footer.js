ce91627c98282e9d2eb9e99b28d67d15fd5bc501
rojimkolwey <rodgersmkolwe@gmail.com>
2026-04-03 18:11:34 +0300

    styling

--- myapp/src/components/Footer.js
+++ myapp/src/components/Footer.js
@@ -1,6 +1,15 @@
 function Footer() {
+  const footerStyle = {
+    marginTop: '20px',
+    padding: '20px',
+    backgroundColor: '#282c34',
+    color: 'white',
+    textAlign: 'center',
+    borderRadius: '8px'
+  };
+
   return (
-    <div>
+    <div style={footerStyle}>
       <p>Made by Rodgers © 2025</p>
     </div>
   );
